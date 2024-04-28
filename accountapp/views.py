64c721b2cb994a04ea6fbbf0cce60f413ee28f33
sayhello06 <sayhello06@naver.com>
2024-04-29 05:39:39 +0900

    GET_POST ver 0.1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
-def hello_world(request) :
-    return HttpResponse('Hello world!')
+def hello_world(request):
+    if request.method == "POST":
 
+        temp = request.POST.get('input-test')
+        return render(request, 'accountapp/hello_world.html', context={'text': temp})
+
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
