acb7088a88f79f095293ae59b8657cc40f1e472d
sayhello06 <sayhello06@naver.com>
2024-05-29 11:02:18 +0900

    FullCalendar Code added

---
+++ calendarApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import calendar
+from calendarApp import views
+
+#app_name = "calendarApp"
+
+urlpatterns = [
+    path('', calendar, name='calendarApp'),
+    path('all_events/', views.all_events, name='all_events'),
+    path('add_event/', views.add_event, name='add_event'),
+    path('update/', views.update, name='update'),
+    path('remove/', views.remove, name='remove'),
+]
