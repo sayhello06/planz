2000d58b7f6946d9d840d1d52955ef0b2c234065
sayhello06 <sayhello06@naver.com>
2024-06-12 11:58:44 +0900

    account_book remake ver0.1

--- accountbookApp/models.py
+++ accountbookApp/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 class Transaction(models.Model):
-    TRANSACTION_TYPES = [
+    TRANSACTION_TYPES = (
         ('income', 'Income'),
         ('expense', 'Expense'),
-    ]
-    title = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    )
     date = models.DateField()
-    category = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPES, default='income')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=255, null=True)
 
     def __str__(self):
-        return f"{self.title} - {self.amount} ({self.transaction_type})"
+        return f"{self.get_type_display()} - {self.amount}"
